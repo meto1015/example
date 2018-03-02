19b34e955673966b5022157107aa9befaf32fe2d
meto1015 <tobi@Tobiass-MacBook-Pro.local>
2018-03-03 00:26:31 +0100

    ; fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/webhook/', function(req, res) {
 
 app.post('/webhook/', function(req, res) {
   let messaging_events = req.body.entry[0].messaging
-  for (let i = 0; i < messaging_events.length; i++;) {
+  for (let i = 0; i < messaging_events.length; i++) {
     let event = messaging_events[i]
     let sender = event.sender.id
     if (event.message && event.message.text) {
